01c26ed1947bc65dfe36f672fc88dcf02836873c
Charlie Theodore <charliestableford2@gmail.com>
2020-05-22 10:13:31 -0700

    final

--- finalSubmission.js
+++ finalSubmission.js
@@ -1,24 +1,24 @@
 //PRODUCT LISTING EXCERCISE
-//task 1
+//Task 1
 const hide = document.querySelector('.coltitle');
 window.onload = (event) => {
 hide.style.display = 'none'
 };
 
-//task 2
+//Task 2
 const banner = document.querySelector('.image-element__wrap');
 banner.style.height = '20vh';
 
-//task 3
+//Task 3
 /*provide 2 aditional scenarios that could be A/B tested on this page to help a prospective customer convert to a sale.
 Scenario one - I would make the banner smaller and make the text of ‘everything up to 40% off’ more visible. I would also, but not at the same time for testing purposes, take away the text of 'All Sex Toys'. My hypothesis in this scenario being that it would take the user less time to navigate to the products.
 Scenario two - Make the filter button more prominent in comparison to the most reviews drop down menu.
-My hypothesis being, based on the heat map it is clicked double the amount as the reviews button and if more prominent would allow users to more quickly access their product of choice. 
+My hypothesis being, based on the heat map it is clicked double the amount as the reviews button and if more prominent would allow users to more quickly access their product of choice in mobile view.
 */
 
 //PRODUCT DETAIL EXCERCISE
-//task 1
-let primaryLogo = document.querySelector('.primary-logo');
+//Task 1
+const primaryLogo = document.querySelector('.primary-logo');
 window.onload = (event) => {
     primaryLogo.style.display = 'none';
     };
@@ -67,6 +67,7 @@ function variantOne(searchBar){
 
 //Challenges faced 
 // I started with going about this as I had done in the previous tasks, but I wanted to show diverse learning so I went about it the second way. But when testing I could not implelement this so it was more of a theory. The third way I was tryingto create an array of an obj and then loop over it and create new values for the searchBar variable defined above. I also thought about making this into a function. The last small function is to demonstrate thought process if this were a testing scenario. It would be easiest for traffic to be pointed in one direction through a function if they wanted to see the screen without the logo and a smaller search bar and the control group to reamin the same. 
+// One last way to go about this would be building an alternative html with back ticks and ES6. If placed in a function, x amount of viewers would see this and y amount of viewers would see the page as is.
 // I always try to start out with something that I know will work within my time frame before doing something that I am not as comfortable with because I always want something that is functioning to fall back on. I also would go by the companies best practices on this!
 
 // Task 2
@@ -106,19 +107,19 @@ function priceVariant(price){
 /*
 Structuring of a test:
 
-Observation: Based on the graph above there is a high abandoment of the basket without proceeding to checkout. 
+Observation: Based on the graph depicted there is a high abandoment of the basket without proceeding to checkout. 
 Hypothesis: If the checkout cart icon was in the bottom right hand corner when you scrolled it would remind users that there was something in their cart. 
 Variations: There would be two varations of this. 50% of users would see the page as is, this would allow a control group for our test to be run against. I would have the other 50% see the cart at the bottom right hand corner. 
-This test would run for two weeks. Important to note that every test that I run will run for the entrie time laid as out to make sure it is viable. 
+This test would run for two weeks. Important to note that every test that I run will run for the entire time laid as out to make sure it is viable. 
 I would also want this test to be statistically signifigant. In order to see this I would want to see a 5% increase in folks heading to the checkout. 
-    Population: Our population are folks that have added something to their cart.
+    Population: Our population are users that have added something to their cart.
     Sample: This represents the participants in the test, the folks who are visiting the site and adding an item to their cart for the duration og the test. Our test is attempting to estimate the behaviour of our sample population. The two groups (control and sample) will have an identical experience other than the movement of the cart icon on scroll. 
     Predicted outcomes: 
         1) Our test runs and there is a 5% increase in checkouts. 
         2) There is no detectable difference and our test fails - back to the drawing board!
         3) There is an increase in checkouts but our test does not see it. Maybe run again with a higher margin.
         4) There is no increase but the test believes there was. Was the sample size predicative of the entire population? Was that able to be measured within a 2 week time span?
-    I would use a sample size caluculator to measure the sample size needed. This will take the baseline conversion rate (I would want to set this at 5%). I would want the statistical signifgance to be high 90%-95%. If there was a baseline conversion rate of 5% and a mionimum detectable effect of 1% we will need a sample size f 12,866. If, based on the past statistics of the website (as the graph shown in the test does not show time), the number needed to be tested was to high I would decrease the statistcal power and increase the minimum detectable effect - allowing for more error but a smaller population test size. 
+    I would use a sample size caluculator to measure the sample size needed. This will take the baseline conversion rate (I would want to set this at 5%). I would want the statistical signifgance to be high 90%-95%. If there was a baseline conversion rate of 5% and a minimum detectable effect of 1% we will need a sample size of 12,866. If, based on the past statistics of the website (as the graph shown in the test does not show time), the number needed to be tested was too high I would decrease the statistical power and increase the minimum detectable effect - allowing for more error but a smaller population test size. 
     If the baseline conversion rate was at 5% then, based on the graph, I would want the test group to have an increased checkout rate of 11% minimum while the control group stayed at 6% of folks heading to the checkout page. 
     If this test worked, and the expected outcome of having a 5% increase was reached the test would be a success.
     Regardless of the success of the test I would write up a conclusion outlining the test for the marketing, design and engineering team.
